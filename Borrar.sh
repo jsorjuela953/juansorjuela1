cf12ebab3b7c22eceaf7f3df950a6205e59bf1cc
jsorjuela953 <js.orjuela953@uniandes.edu.co>
2020-10-07 23:57:01 -0500

    Add files via upload

--- Borrar.sh
+++ Borrar.sh
@@ -1,3 +1,3 @@
-hadoop fs -rmkdir /user/bigdata08/Taller1
+hadoop fs -rmdir /user/bigdata08/Taller1
 rm WordCount.jar
 wget https://github.com/jsorjuela953/juansorjuela1.github.io/raw/gh-pages/WordCount.jar
